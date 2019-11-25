a017d91f0bd0d98129add3498610f17c212f148f
Shohail Ahmed <ahmedsu868@gmail.com>
2019-11-26 02:05:33 +0530

    Updates

---
+++ precache-manifest.f0f20bdfca745e21bc411cb6651a5fe5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15f8888bf3f19b32418d01b854a6953a",
+    "url": "/mocks/index.html"
+  },
+  {
+    "revision": "b276bf4f7121edf9dbee",
+    "url": "/mocks/static/css/2.cbb01bb0.chunk.css"
+  },
+  {
+    "revision": "2772fe831f68ca37f042",
+    "url": "/mocks/static/css/main.866afdba.chunk.css"
+  },
+  {
+    "revision": "b276bf4f7121edf9dbee",
+    "url": "/mocks/static/js/2.2f333875.chunk.js"
+  },
+  {
+    "revision": "2772fe831f68ca37f042",
+    "url": "/mocks/static/js/main.40622315.chunk.js"
+  },
+  {
+    "revision": "74fef5f9221eb27d4941",
+    "url": "/mocks/static/js/runtime-main.4237a8ab.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/mocks/static/media/logo.25bf045c.svg"
+  }
+]);
